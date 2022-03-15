aa8f6e909df6758598834bd635b8b607d8ae7325
Eduard  Jacobs <eduardjacobs@Eduards-MacBook-Pro.local>
2022-03-15 16:13:39 -0700

    basic styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
 import { useState } from "react";
 import axios from "axios";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+`;
+
+const Wrapper = styled.div`
+  text-align: center;
+  margin-top: 5rem;
+  width: 100%;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+  width: 250px;
+  border-radius: 4px;
+  padding: 0.5rem 1rem;
+  border: 2px solid #1aa1e5;
+`;
 
 const Home = () => {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -29,18 +49,19 @@ const Home = () => {
     }
   };
   return (
-    <div>
-      <div>
-        <label htmlFor="uploadimage">Choose file to upload</label>
-        <input
-          type="file"
-          accept=".png"
-          name="uploadimage"
-          onChange={onHandleChange}
-        />
-      </div>
-      <button onClick={onHandleSubmit}>Upload</button>
-    </div>
+    <Container>
+      <Wrapper>
+        <div>
+          <input
+            type="file"
+            accept=".png"
+            name="uploadimage"
+            onChange={onHandleChange}
+          />
+        </div>
+        <Button onClick={onHandleSubmit}>Upload</Button>
+      </Wrapper>
+    </Container>
   );
 };
 
